86c46ca680482ee50c93baf534867834fee33be9
justtheboss97 <justin.bon88@gmail.com>
2021-05-18 23:14:50 +0200

    User now sees graph on page load

--- app.py
+++ app.py
@@ -11,7 +11,23 @@ print('hello world')
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+
+    result =  np.load('stijnshit\\todaysnumbers.npy',allow_pickle=True).item()
+    
+    # temp_list = [0 for x in range(10)]
+
+    # for item in list(result.values()):
+    #     temp_list[item - 1] += 1
+
+    temp_dict = {}
+
+    for item in list(result.values()):
+        if item in temp_dict:
+            temp_dict[item] += 1
+        else:
+            temp_dict[item] = 1
+
+    return render_template('index.html', data = temp_dict)
 
 
 @socketio.on('request data')
